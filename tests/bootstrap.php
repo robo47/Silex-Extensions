b1c8d8a2a72764d6555e1a30fb446b0a4d1f7c50
Benjamin Steininger <robo47@robo47.net>
2012-03-09 13:22:03 +0100

    Using Composer for autoload if available

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,15 @@
 <?php
 
-require_once __DIR__ . '/../silex.phar';
-
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
-$loader = new UniversalClassLoader();
-$loader->registerNamespace('SilexExtension', __DIR__ . '/../src');
-$loader->register();
+require_once __DIR__ . '/../silex.phar';
+
+if (file_exists(__DIR__ . '/../vendor/.composer/autoload.php')) {
+    require_once __DIR__ . '/../vendor/.composer/autoload.php';
+} else {
+
+    $loader = new UniversalClassLoader();
+    $loader->registerNamespace('SilexExtension', __DIR__ . '/../src');
+    $loader->register();
+
+}
