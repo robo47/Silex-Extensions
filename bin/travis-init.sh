93a2a1863009c2ad24c8db3f82e3a034356934cf
Benjamin Steininger <robo47@robo47.net>
2012-03-09 14:41:54 +0100

    memcached.so

--- bin/travis-init.sh
+++ bin/travis-init.sh
@@ -4,7 +4,7 @@ sudo apt-get install libmemcached-dev
 curl -s http://pecl.php.net/get/memcached-2.0.1.tgz > memcached-2.0.1.tgz
 tar -xzf memcached-2.0.1.tgz
 sh -c "cd memcached-2.0.1 && phpize && ./configure && make && sudo make install"
-echo "extension=mongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+echo "extension=memcached.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
 # installing mongo extension
 curl -s http://pecl.php.net/get/mongo-1.2.9.tgz > mongo-1.2.9.tgz
