90b279f679d7f541dce6e4e731f877fd6d8d3db8
Benjamin Steininger <robo47@robo47.net>
2012-03-09 14:49:31 +0100

    memcache instead of memcached

--- bin/travis-init.sh
+++ bin/travis-init.sh
@@ -1,10 +1,10 @@
 sudo apt-get install libmemcached-dev
 
-# installing memcached extension
-curl -s http://pecl.php.net/get/memcached-2.0.1.tgz > memcached-2.0.1.tgz
-tar -xzf memcached-2.0.1.tgz
-sh -c "cd memcached-2.0.1 && phpize && ./configure && make && sudo make install"
-echo "extension=memcached.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+# installing memcache extension
+curl -s http://pecl.php.net/get/memcache-2.2.6.tgz > memcache-2.2.6.tgz
+tar -xzf memcache-2.2.6.tgz
+sh -c "cd memcache-2.2.6 && phpize && ./configure && make && sudo make install"
+echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
 # installing mongo extension
 curl -s http://pecl.php.net/get/mongo-1.2.9.tgz > mongo-1.2.9.tgz
